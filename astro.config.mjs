d57587106980b5b6c4ca9fb8a0edd7d88cdc00c6
Jakub Dorničák <jakub.dornicak@blindspot.ai>
2024-03-07 08:18:11 +0100

    feat: 🎸 Site change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: 'https://dornicakjakub.cz',
   server: {
     port: 8080,
     host: '0.0.0.0'
